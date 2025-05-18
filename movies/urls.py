f3e2cc3afd30d387086e450e4fa8d84adcb22f8b
Aziz DEVECİ <230541156@firat.edu.tr>
2025-05-18 13:21:32 +0300

    Update urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
+# http://127.0.0.1:8000/
+# http://127.0.0.1:8000/home
+# http://127.0.0.1:8000/movies
+# http://127.0.0.1:8000/movies/3
+# http://127.0.0.1:8000/movies/walking-dead
 
-    #http://127.0.0.1:8000/
-    #http://127.0.0.1:8000/home
-    #http://127.0.0.1:8000/movies
-
-
-    path("",views.home),
-    path("home",views.home),
-    path("movies",views.movies)
-
-
-
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("home", views.home),
+    path("movies", views.movies, name="movies"),
+    path("movies/<int:id>", views.moviedetails, name="details"),
 ]
